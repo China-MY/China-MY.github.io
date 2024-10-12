62a8846af96b13ee0edbe23a480e67d804789660
myxz <1792943568@qq.com>
2024-10-12 12:52:54 +0800

    Site updated: 2024-10-12 12:52:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/10/12/hello-world/"];function toRandomPost(){
+var posts=["2024/10/12/前端开发/","2024/10/12/生活日常/","2024/10/12/大学生涯/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
